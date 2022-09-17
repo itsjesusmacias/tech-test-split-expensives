b1df4051be0b079e4d7da0a7e0045fd226c53325
Yisus <jesus.macias@devo.com>
2022-09-17 12:24:29 +0200

    initializing the project 🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import Head from "next/head";
+
+import styles from "../styles/Home.module.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Split bills</title>
+        <meta
+          name="description"
+          content="App to track bills and other shared expenses"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.linkedin.com/in/jmfelix/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by Jesús 👨🏽‍💻
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
